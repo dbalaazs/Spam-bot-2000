fadb67d960fab32ad8831f34c26b9cfa6069e594
dbalaazs <dbalaazs@gmail.com>
2020-12-17 11:57:28 +0100

    Update Commands.cs

--- Discord Bot/Modules/Commands.cs
+++ Discord Bot/Modules/Commands.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -13,34 +13,15 @@ namespace DiscordBot.Modules
     public class Commands : ModuleBase<SocketCommandContext>
     {
         [Command("sp")]
-        
+
         public async Task Sp(string message, string number)
         {
-            await ReplyAsync("Got it boss :thumbsup:");
             bool isNum = Int32.TryParse(number, out int result);
             if (isNum == true)
             {
                 if (result <= 100)
                 {
-                    while (result > 0)
-                    {
-                        await ReplyAsync(message);
-                        Thread.Sleep(1000);
-                        result--;
-                    }
-                }
-                else { await ReplyAsync("I can't shitpost over 100 messages bruv"); }
-            }      
-        }
-        [Command("shitpost")]
-        public async Task ShitPost(string message, string number)
-        {
-            await ReplyAsync("Got it boss :thumbsup:");
-            bool isNum = Int32.TryParse(number, out int result);
-            if (isNum == true)
-            {
-                if (result <= 100)
-                {
+                    await ReplyAsync("Got it boss :thumbsup:");
                     while (result > 0)
                     {
                         await ReplyAsync(message);
@@ -50,6 +31,33 @@ namespace DiscordBot.Modules
                 }
                 else { await ReplyAsync("I can't shitpost over 100 messages bruv"); }
             }
+            else { await ReplyAsync("You messed up something in the request boss"); }
+        }
+        [Command("shitpost")]
+        public async Task ShitPost(string message, string number)
+        {
+            bool isNum = Int32.TryParse(number, out int result);
+            if (isNum == true)
+            {
+                if (result <= 100)
+                {
+                    await ReplyAsync("Got it boss :thumbsup:");
+                    while (result > 0)
+                    {
+                        await ReplyAsync(message);
+                        Thread.Sleep(1000);
+                        result--;
+                    }
+                }
+                else { await ReplyAsync("I can't shitpost over 100 messages bruv"); }
+            }
+            else { await ReplyAsync("You messed up something in the request boss"); }
+        }
+        [Command("help")]
+        public async Task Help()
+        {
+            await ReplyAsync("You can say ~sp or ~shitpost, and then put in your message (be careful not to put any spaces), then put in how many times to shitpost it. You can't go over 100!");
+            await ReplyAsync("For example: ~sp fuck-you 20");
         }
     }
 }
